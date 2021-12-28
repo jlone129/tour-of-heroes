532b1efa3ec18d85a3a3cf04fb28e6bf9db981bf
John Lonetree <john.lonetree@kin.com>
2021-12-28 13:45:27 -0600

    adding dashboard and routing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
+  }
+
 }
